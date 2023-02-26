f698f0e7ef5b880f5d2fb864eadca6a575073baf
Cesar Vega <g.vega.cl@gmail.com>
2023-02-25 21:35:05 -0500

    set up react native and rneui

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Cards from './components/Card/Card';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Cards />
       <StatusBar style="auto" />
     </View>
   );
